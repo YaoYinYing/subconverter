f5bc0a5bba3b3436484c36f7f9bc43074fffa123
YaoYinYing <33014714+YaoYinYing@users.noreply.github.com>
2024-07-06 20:07:45 +0800

    Update build.alpine.release.sh

--- scripts/build.alpine.release.sh
+++ scripts/build.alpine.release.sh
@@ -47,9 +47,9 @@ rm subconverter
 # shellcheck disable=SC2046
 g++ -o base/subconverter $(find CMakeFiles/subconverter.dir/src/ -name "*.o")  -static -lpcre2-8 -lyaml-cpp -L/usr/lib64 -lcurl -lmbedtls -lmbedcrypto -lmbedx509 -lz -l:quickjs/libquickjs.a -llibcron -O3 -s
 
-python3 -m ensurepip
-python3 -m pip install gitpython
-python3 scripts/update_rules.py -c scripts/rules_config.conf
+# python3 -m ensurepip
+# python3 -m pip install gitpython
+# python3 scripts/update_rules.py -c scripts/rules_config.conf
 
 cd base
 chmod +rx subconverter
